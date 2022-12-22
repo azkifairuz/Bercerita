401487732a08fe95ae059c81f715a05543a0bac6
trian damai <triannurizkillah@gmail.com>
2022-12-22 12:53:05 +0700

    feat: add dynamic question

---
+++ app/src/main/java/com/javfairuz/bercerita/question/DataQuestion.kt
@@ -0,0 +1,64 @@
+package com.javfairuz.bercerita.question
+
+val question = listOf(
+    QuestionModel(
+        question = "1.Dalam sebulan terakhir, seberapa sering kamu menjadi mudah tersingung",
+        options = listOf(
+            OptionsModel(
+                answer = "Pernah",
+                key = "pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Belum Pernah",
+                key = "belum_pernah",
+                score = 1
+            )
+        )
+    ),
+    QuestionModel(
+        question = "2.Dalam sebulan terakhir, seberapa ",
+        options = listOf(
+            OptionsModel(
+                answer = "Ga ",
+                key = "pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Yoi",
+                key = "belum_pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Ga ",
+                key = "pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Yoi",
+                key = "belum_pernah",
+                score = 1
+            ),
+        )
+    ),
+    QuestionModel(
+        question = "3.Dalam sebulan terakhir, seberapa ",
+        options = listOf(
+            OptionsModel(
+                answer = "Ga ",
+                key = "pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Yoi",
+                key = "belum_pernah",
+                score = 1
+            ),
+            OptionsModel(
+                answer = "Yoi",
+                key = "belum_pernah",
+                score = 1
+            )
+        )
+    )
+)
--- app/src/main/java/com/javfairuz/bercerita/question/PageQuestion.kt
+++ app/src/main/java/com/javfairuz/bercerita/question/PageQuestion.kt
@@ -57,68 +57,6 @@ fun PageQuestion(navHostController: NavHostController = rememberNavController())
         mutableStateOf(0)
     }
 
-    var question = listOf(
-        QuestionModel(
-            question = "1.Dalam sebulan terakhir, seberapa sering kamu menjadi mudah tersingung",
-            options = listOf(
-                OptionsModel(
-                    answer = "Pernah",
-                    key = "pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Belum Pernah",
-                    key = "belum_pernah",
-                    score = 1
-                )
-            )
-        ),
-        QuestionModel(
-            question = "2.Dalam sebulan terakhir, seberapa ",
-            options = listOf(
-                OptionsModel(
-                    answer = "Ga ",
-                    key = "pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Yoi",
-                    key = "belum_pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Ga ",
-                    key = "pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Yoi",
-                    key = "belum_pernah",
-                    score = 1
-                ),
-            )
-        ),
-        QuestionModel(
-            question = "3.Dalam sebulan terakhir, seberapa ",
-            options = listOf(
-                OptionsModel(
-                    answer = "Ga ",
-                    key = "pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Yoi",
-                    key = "belum_pernah",
-                    score = 1
-                ),
-                OptionsModel(
-                    answer = "Yoi",
-                    key = "belum_pernah",
-                    score = 1
-                )
-            )
-        )
-    )
 
     fun nextQuestion() {
         if (currentQuestion != question.size -1) {
