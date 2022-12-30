8dc9687f9724a96b2bade2273c7504d6b5068e72
azkifairuz <azkiajmal@gmail.com>
2022-12-31 02:54:27 +0700

    styling on boarding

--- app/src/main/java/com/javfairuz/bercerita/OnBoardingItems.kt
+++ app/src/main/java/com/javfairuz/bercerita/OnBoardingItems.kt
@@ -1,3 +1,14 @@
 package com.javfairuz.bercerita
 
-data class OnBoardingItems(var image : Int,var title: String, var desc :String)
+class OnBoardingItems(var image : Int,var title: String, var desc :String)
+{
+    companion object {
+        fun get():List<OnBoardingItems>{
+            return listOf(
+                OnBoardingItems(R.drawable.logo_on_boarding,"becerita", "bebas bercerita apa saja"),
+                OnBoardingItems(R.drawable.logo_on_boarding,"becerta 2", "bebas bercerita apa saja 2"),
+                OnBoardingItems(R.drawable.logo_on_boarding,"becerta 3", "bebas bercerita apa saja 3")
+            )
+        }
+    }
+}
